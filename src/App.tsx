f4fd3285189f5c13de430ec5e0de5f90fb0b001c
piyushkushwaha977 <piyushkushwaha977@gmail.com>
2025-08-23 19:14:26 +0530

    Add intialized Project with tailwind & TS configuration

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div className=" bg-gray-800 text-gray-400 w-full h-screen">
+    <h2 className=' text-center text-7xl font-bold'> React With Typescript </h2>
+    </div>
+  )
+}
+
+export default App
