d5155630c4be2507cd2e587b51372f0f9cea4621
piyushkushwaha977 <piyushkushwaha977@gmail.com>
2025-08-23 23:01:28 +0530

    Add Routing using react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,30 @@
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from 'react-router'
+import { RouterProvider } from 'react-router/dom'
 import './index.css'
 import App from './App.tsx'
 
+export function Layout(){
+  return(
+    <div>
+      <h1> Hii i am Layout</h1>
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>
+  },
+  {
+    path:"/layout",
+    element:<Layout/>
+  }
+])
+
 createRoot(document.getElementById('root')!).render(
   <>
-    <App />
+    <RouterProvider router={router} />
   </>,
 )
